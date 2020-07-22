9d61b62500058f4145f55c4c72865ba808289cfd
AmauriMarcos <amaurisantos.m@gmail.com>
2020-07-22 18:01:43 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+      <div v-for="(post, i) in posts" :key="i">
+          <p>{{post.title}}</p>
+          <p>{{post.image.url}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+   async asyncData(){
+     const res = await axios.get("https://test-santos.herokuapp.com/posts")
+     return{posts: res.data}
+   }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+</style>
